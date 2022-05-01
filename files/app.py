43a71c4f2e3b41d79a7da60d60c0ad8d52f62e50
Adam Littrell <adam.k.littrell@gmail.com>
2022-05-01 12:38:00 -0400

    first steps with flask. I think I have the basic building blocks for this. It is not going to be pretty but it will be functional. Concerned that the tables showing all the job postings that were scraped for a given day are not rending the a ref tags in the posting and content columns. Maybe its the encoding, maybe its because there is not a space between the <th> and <a>. I will figure it out. Next steps are to either modify the code to put files where they need to go or to build the customizations page.

--- files/app.py
+++ files/app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField, SelectField, DateField, SubmitField)
+from wtforms import StringField, TextAreaField, IntegerField, BooleanField, FormField, SelectField, FieldList, SubmitField
 from wtforms.validators import InputRequired, Length
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+import time
+import os
+import job_scraper
+from collections import namedtuple
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'makesuretochangethisbeforehittingcommit'
+executors = {
+    'default': ThreadPoolExecutor(16),
+    'processpool': ProcessPoolExecutor(4)
+}
+sched = BackgroundScheduler(timezone='America/New_York', executors=executors)
+
+#sched.add_job(run_job_scraper, 'interval', minutes=120)
+def run_job_scraper():
+    os.chdir("/app")
+    job_scraper.main()
+
+
+def process_customizations(get=None, post=None, form=None):
+
 
 
 @app.route('/', methods=('GET', 'POST'))
@@ -26,5 +46,56 @@ def jobpost(date, jobposting):
     print(date + '/' + jobposting)
     return render_template(date + '/' + jobposting + '/content.html')
 
+
+class SearchTermRow(FlaskForm):
+    search_term_label = StringField()
+    delete_btn = SubmitField(label='Delete')
+
+
+class SearchTermForm(FlaskForm):
+    rows = FieldList(FormField(SearchTermRow), min_entries=0)
+    add_row = SubmitField(label='Add Row')
+    confirm = SubmitField(label='Confirm')
+
+
+@app.route('/customizations', methods=['GET'])
+def customizations():
+    search_term = namedtuple('Terms', ['search_term_label'])
+    s1 = search_term('1')
+    s2 = search_term('2')
+    s3 = search_term('3')
+    s4 = search_term('4')
+    output = "<h1>"
+    #form = SearchTermForm()
+    data = {'rows': [s1, s2, s3, s4]}
+    # if form.add_row.data:
+    #     s5 = search_term('5')
+    #     data = {'rows': [s1, s2, s3, s4, s5]}
+    #     #form = SearchTermForm(data=data)
+    # if form.confirm.data:
+    #     for field in form.rows:
+    #         output += field.search_term_label.data + ' <br />'
+    #     output += '</h1>'
+    #     return output
+    form = SearchTermForm(data=data)
+    return render_template('customizations.html', form=form)
+
+
+@app.route('/customizations1', methods=['POST'])
+def customizations1():
+    print("in customizations 1")
+    r = request
+    return redirect(url_for('customizations'))
+
+
 if __name__ == '__main__':
-    app.run()
+    #sched.start()
+    app.debug = True
+    app.run(host="0.0.0.0")
+
+
+# https://blog.jcharistech.com/2019/12/12/how-to-render-markdown-in-flask/
+
+# Known bugs
+# when you click on the content button, it takes you to a url that ends in /content.html and we need to remove that part
+# need a documentation page
